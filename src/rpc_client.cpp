82c8825d2c59d8b5dbb2c966b7766f2ffb7abf30
BHD Dev <dev@bhd.one>
2023-03-05 21:10:09 +0800

    Refactor RPCClient.

--- src/rpc_client.cpp
+++ src/rpc_client.cpp
@@ -1,13 +1,32 @@
 #include "rpc_client.h"
 
+#include <curl/curl.h>
+
 #include <fstream>
 #include <iostream>
 
 #include <filesystem>
 namespace fs = std::filesystem;
 
+Json::Value MakeArg(std::string const& str)
+{
+    return Json::Value(static_cast<Json::String>(str));
+}
+
+Json::Value MakeArg(std::string_view str)
+{
+    return Json::Value(static_cast<Json::String>(str));
+}
+
+Json::Value MakeArg(Bytes const& data)
+{
+    return Json::Value(BytesToHex(data));
+}
+
 RPCClient::RPCClient(bool no_proxy, std::string url, std::string const& cookie_path_str)
-    : m_no_proxy(no_proxy), m_url(std::move(url)) {
+    : m_no_proxy(no_proxy)
+    , m_url(std::move(url))
+{
     if (cookie_path_str.empty()) {
         throw std::runtime_error("cookie is empty, cannot connect to btchd core");
     }
@@ -32,12 +51,58 @@ RPCClient::RPCClient(bool no_proxy, std::string url, std::string const& cookie_p
 }
 
 RPCClient::RPCClient(bool no_proxy, std::string url, std::string user, std::string passwd)
-    : m_no_proxy(no_proxy), m_url(std::move(url)), m_user(std::move(user)), m_passwd(std::move(passwd)) {}
+    : m_no_proxy(no_proxy)
+    , m_url(std::move(url))
+    , m_user(std::move(user))
+    , m_passwd(std::move(passwd))
+{
+}
 
-void RPCClient::BuildRPCJson(Json::Value& params, std::string const& val) { params.append(val); }
-
-void RPCClient::BuildRPCJson(Json::Value& params, Bytes const& val) { params.append(BytesToHex(val)); }
-
-void RPCClient::BuildRPCJson(Json::Value& params, bool val) { params.append(Json::Value(val)); }
-
-void RPCClient::BuildRPCJsonWithParams(Json::Value& out_params) {}
+RPCClient::Result RPCClient::SendMethod(bool no_proxy, std::string const& method_name, Json::Value const& params)
+{
+    Json::Value root;
+    root["jsonrpc"] = "2.0";
+    root["method"] = method_name;
+    root["params"] = params;
+    // Invoke curl
+    HTTPClient client(m_url, m_user, m_passwd, no_proxy);
+    std::string send_str = root.toStyledString();
+    PLOG_DEBUG << "sending: `" << send_str << "`";
+    int code;
+    std::string err_str;
+    std::tie(code, err_str) = client.Send(send_str);
+    if (code != CURLE_OK) {
+        std::stringstream ss;
+        ss << "RPC command error `" << method_name << "`: " << err_str;
+        throw NetError(ss.str().c_str());
+    }
+    // Analyze the result
+    Bytes received_data = client.GetReceivedData();
+    if (received_data.empty()) {
+        throw NetError("empty result from RPC server");
+    }
+    char const* psz = reinterpret_cast<char const*>(received_data.data());
+    Json::Value res;
+    Json::CharReaderBuilder builder;
+    std::shared_ptr<Json::CharReader> reader(builder.newCharReader());
+    std::string errs;
+    if (!reader->parse(psz, psz + strlen(psz), &res, &errs)) {
+        throw Error("cannot parse the result from rpc server");
+    }
+    // Build result and return
+    PLOG_DEBUG << "received: `" << res.toStyledString() << "`";
+    Result result;
+    if (res.isMember("result")) {
+        result.result = res["result"];
+    }
+    if (res.isMember("error") && !res["error"].isNull()) {
+        Json::Value errorJson = res["error"];
+        int code = errorJson["code"].asInt();
+        std::string msg = errorJson["message"].asString();
+        throw RPCError(code, msg);
+    }
+    if (res.isMember("id") && res["id"].isInt()) {
+        result.id = res["id"].asInt();
+    }
+    return result;
+}
--- src/rpc_client.h
+++ src/rpc_client.h
@@ -3,38 +3,78 @@
 
 #include <curl/curl.h>
 #include <plog/Log.h>
+
 #include <json/value.h>
 #include <json/reader.h>
 
 #include <cstdint>
+
 #include <string>
+#include <string_view>
+
+#include <type_traits>
 
 #include "http_client.h"
 
 #include "utils.h"
+#include "types.h"
 
-class Error : public std::runtime_error {
+class Error : public std::runtime_error
+{
 public:
-    explicit Error(char const* msg) : std::runtime_error(msg) {}
+    explicit Error(char const* msg)
+        : std::runtime_error(msg)
+    {
+    }
 };
 
-class NetError : public Error {
+class NetError : public Error
+{
 public:
-    explicit NetError(char const* msg) : Error(msg) {}
+    explicit NetError(char const* msg)
+        : Error(msg)
+    {
+    }
 };
 
-class RPCError : public Error {
+class RPCError : public Error
+{
 public:
-    RPCError(int code, std::string msg) : Error(msg.c_str()), m_code(code), m_msg(std::move(msg)) {}
+    RPCError(int code, std::string msg)
+        : Error(msg.c_str())
+        , m_code(code)
+        , m_msg(std::move(msg))
+    {
+    }
 
-    int GetCode() const { return m_code; }
+    int GetCode() const
+    {
+        return m_code;
+    }
 
 private:
     int m_code;
     std::string m_msg;
 };
 
-class RPCClient {
+Json::Value MakeArg(std::string const& str);
+
+Json::Value MakeArg(std::string_view str);
+
+template <typename T> Json::Value MakeArg(T int_val, std::is_integral<T>&)
+{
+    return Json::Value(static_cast<Json::Int64>(int_val));
+}
+
+Json::Value MakeArg(Bytes const& data);
+
+template <int N> Json::Value MakeArg(std::array<uint8_t, N> const& data)
+{
+    return MakeArg(MakeBytes(data));
+}
+
+class RPCClient
+{
 public:
     struct Result {
         Json::Value result;
@@ -46,97 +86,21 @@ public:
 
     RPCClient(bool no_proxy, std::string url, std::string user, std::string passwd);
 
-    template <typename... T>
-    Result Call(std::string const& method_name, T&&... vals) {
-        return SendMethod(m_no_proxy, method_name, std::forward<T>(vals)...);
+    template <typename... T> Result Call(std::string const& method_name, T&&... vals)
+    {
+        Json::Value params;
+        MakeParams(params, std::forward<T>(vals)...);
+        return SendMethod(m_no_proxy, method_name, params);
     }
 
 private:
-    void BuildRPCJson(Json::Value& params, std::string const& val);
-
-    void BuildRPCJson(Json::Value& params, Bytes const& val);
-
-    void BuildRPCJson(Json::Value& params, bool val);
-
-    template <size_t N>
-    void BuildRPCJson(Json::Value& params, std::array<uint8_t, N> const& val) {
-        BuildRPCJson(params, BytesToHex(MakeBytes(val)));
+    template <typename T, typename... Ts> void MakeParams(Json::Value& out_params, T&& val, Ts&&... vals)
+    {
+        out_params.append(MakeArg(std::forward<T>(val)));
+        MakeParams(out_params, std::forward<Ts>(vals)...);
     }
 
-    template <typename T>
-    void BuildRPCJson(Json::Value& params, std::vector<T> const& val) {
-        Json::Value res(Json::arrayValue);
-        for (auto const& v : val) {
-            BuildRPCJson(res, v);
-        }
-        params.append(res);
-    }
-
-    template <typename T>
-    void BuildRPCJson(Json::Value& params, T&& val) {
-        params.append(std::forward<T>(val));
-    }
-
-    void BuildRPCJsonWithParams(Json::Value& out_params);
-
-    template <typename V, typename... T>
-    void BuildRPCJsonWithParams(Json::Value& outParams, V&& val, T&&... vals) {
-        BuildRPCJson(outParams, std::forward<V>(val));
-        BuildRPCJsonWithParams(outParams, std::forward<T>(vals)...);
-    }
-
-    template <typename... T>
-    Result SendMethod(bool no_proxy, std::string const& method_name, T&&... vals) {
-        Json::Value root;
-        root["jsonrpc"] = "2.0";
-        root["method"] = method_name;
-        Json::Value params(Json::arrayValue);
-        BuildRPCJsonWithParams(params, std::forward<T>(vals)...);
-        root["params"] = params;
-        // Invoke curl
-        HTTPClient client(m_url, m_user, m_passwd, no_proxy);
-        std::string send_str = root.toStyledString();
-        PLOG_DEBUG << "sending: `" << send_str << "`";
-        bool succ;
-        int code;
-        std::string err_str;
-        std::tie(succ, code, err_str) = client.Send(send_str);
-        if (!succ) {
-            std::stringstream ss;
-            ss << "RPC command error `" << method_name << "`: " << err_str;
-            throw NetError(ss.str().c_str());
-        }
-        // Analyze the result
-        Bytes received_data = client.GetReceivedData();
-        if (received_data.empty()) {
-            throw NetError("empty result from RPC server");
-        }
-        char const* psz = reinterpret_cast<char const*>(received_data.data());
-        Json::Value res;
-        Json::CharReaderBuilder builder;
-        std::shared_ptr<Json::CharReader> reader(builder.newCharReader());
-        std::string errs;
-        if (!reader->parse(psz, psz + strlen(psz), &res, &errs)) {
-            throw Error("cannot parse the result from rpc server");
-        }
-
-        // Build result and return
-        PLOG_DEBUG << "received: `" << res.toStyledString() << "`";
-        Result result;
-        if (res.isMember("result")) {
-            result.result = res["result"];
-        }
-        if (res.isMember("error") && !res["error"].isNull()) {
-            Json::Value errorJson = res["error"];
-            int code = errorJson["code"].asInt();
-            std::string msg = errorJson["message"].asString();
-            throw RPCError(code, msg);
-        }
-        if (res.isMember("id") && res["id"].isInt()) {
-            result.id = res["id"].asInt();
-        }
-        return result;
-    }
+    Result SendMethod(bool no_proxy, std::string const& method_name, Json::Value const& params);
 
 private:
     bool m_no_proxy;
